949c15c92ef31d70358a6285d5dc74140356a751
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-10 14:36:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,9 +28,31 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/posts/:id" do
-   @post = Post.find_by_id(params[:id])
-   erb :show
- end
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch "/posts/:id" do
+    post = Post.find_by_id(params[:id])
+    post.name = params[:name]
+    post.content = params[:content]
+    post.save
+
+    redirect :"/posts/#{post.id}"
+  end
+
+  delete "/posts/:id/delete" do
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
 
 
 end
